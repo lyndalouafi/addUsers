ca58fffa6c7a7670522b53ab59b10a485c38193a
lynda louafi <louafilynda5@gmail.com>
2026-03-17 01:49:52 +0100

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
-    mongoose.connect(
-      "mongodb+srv://lynda:Lynda2026db@cluster0.etytbvd.mongodb.net/?appName=Cluster0",
-      {
-        serverSelectionTimeoutMS: 10000,
-      },
-    );
+    mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
